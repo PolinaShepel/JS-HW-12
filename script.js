1ea0d8836bf89513fab70b68394f81fbda22518e
PolinaShepel <118113311+PolinaShepel@users.noreply.github.com>
2022-11-13 22:45:00 +0200

    add

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelectorAll('.btn');
+let currentElem = null;
+
+document.addEventListener('keydown', (event) => {
+    btn.forEach(element => {
+        if (event.key.toUpperCase() === element.innerText || event.key === element.innerText) {
+            if (currentElem) {
+                currentElem.style.backgroundColor = 'black';
+            }
+            currentElem = element;
+            currentElem.style.backgroundColor = 'blue';
+        }
+    })
+});
